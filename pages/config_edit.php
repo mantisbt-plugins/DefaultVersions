b387e877beb054104aaaedfb4eb643f62c6c40e7
spmeesseman <spmeesseman@gmail.com>
2019-08-03 01:05:25 -0400

    featmin: show the success redirect when saving config settings

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -1,5 +1,6 @@
 <?php
 
+form_security_validate( 'plugin_DefaultVersions_config_edit' );
 auth_reauthenticate();
 
 access_ensure_global_level(config_get('manage_plugin_threshold'));
@@ -8,4 +9,11 @@ plugin_config_set('set_product_version', gpc_get_int('set_product_version', 1));
 plugin_config_set('set_target_version', gpc_get_int('set_target_version', 1));
 plugin_config_set('set_target_method', gpc_get_int('set_target_method', 1));
 
-print_successful_redirect(plugin_page('config', TRUE));
+form_security_purge( 'plugin_DefaultVersions_config_edit' );
+
+$t_redirect_url = plugin_page('config', TRUE);
+
+layout_page_header( null, $t_redirect_url );
+layout_page_begin();
+html_operation_successful( $t_redirect_url );
+layout_page_end();
